b47a38276e5f3a46d3df5fce39a016662dd37150
Vincent Toups <toups@email.unc.edu>
2022-02-11 14:15:27 -0500

    Modifications to launch script.

--- docker-console-emacs.sh
+++ docker-console-emacs.sh
@@ -2,7 +2,6 @@
 
 docker run \
        --rm \
-       --gpus device=GPU-60f0b2bf-caf9-a427-0f4f-b50165a07d72\
        --name textgen-emacs \
        -v $(pwd):/home/rstudio/work \
        -it textgen emacs
